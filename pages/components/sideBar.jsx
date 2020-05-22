b599b8c8c781c1269cac35493bda743ce11d4056
blackstormx <blackshtormx@gmail.com>
2020-05-22 20:16:06 +0400

    changes

--- pages/components/sideBar.jsx
+++ pages/components/sideBar.jsx
@@ -1,13 +1,26 @@
 import React, { Component } from "react";
 
+class UserRow extends Component {
+  render() {
+    return (
+      <div className="flex cursor-pointer hover:bg-darkGray-700 px-3 py-2 rounded-md">
+        <div className="user-head-small"></div>
+        <div className="text-darkGray-400 flex-1 flex">
+          <span className="m-auto ml-4">Morgenshtern</span>
+        </div>
+      </div>
+    );
+  }
+}
+
 class SideBar extends Component {
   state = {
     findText: ""
   };
   render() {
     return (
-      <div className="bg-darkGray-800 shadow-inner width-sidebar flex">
-        <div className="height-header flex w-full p-3 py-2">
+      <div className="bg-darkGray-800 width-sidebar flex flex-col">
+        <div className="height-header flex w-full p-3 py-2 border-b-2 border-darkGray-900">
           <input
             type="text"
             value={this.state.findText}
@@ -15,6 +28,9 @@ class SideBar extends Component {
             className="bg-darkGray-900 text-darkGray-400 placeholder-darkGray-400 text-sm px-2 rounded-md outline-none h-8 m-auto w-full"
             onChange={e => this.setState({ findText: e.target.value })}
           />
+        </div>
+        <div className="p-3">
+          <UserRow />
         </div>
       </div>
     );
