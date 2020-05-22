517c2c81df0f47d20d080733ae2c6ca5b1dc47c2
blackstormx <blackshtormx@gmail.com>
2020-05-22 20:40:01 +0400

    design

--- pages/components/sideBar.jsx
+++ pages/components/sideBar.jsx
@@ -8,6 +8,7 @@ class UserDock extends Component {
         <div className="text-darkGray-100 font-medium flex-1 flex">
           <span className="m-auto ml-4">White_Stardust</span>
         </div>
+        <div className=""></div>
       </div>
     );
   }
@@ -32,24 +33,27 @@ class SideBar extends Component {
   };
   render() {
     return (
-      <div className="bg-darkGray-800 width-sidebar flex flex-col select-none">
-        <div className="height-header flex w-full p-3 py-2 border-b-2 border-darkGray-900">
-          <input
-            type="text"
-            value={this.state.findText}
-            placeholder="Find or start a conversation"
-            className="bg-darkGray-900 text-darkGray-400 placeholder-darkGray-400 text-sm px-2 rounded-md outline-none h-8 m-auto w-full"
-            onChange={e => this.setState({ findText: e.target.value })}
-          />
-        </div>
-        <div className="p-3 pt-1 px-2">
-          <span className="text-xs px-3 mt-2 text-darkGray-400 font-medium">
-            DIRECT MESSAGES
-          </span>
-          <UserRow />
-        </div>
-        <div className="bg-darkGray-900 p-3 m-auto mb-0 w-full">
-          <UserDock />
+      <div className="bg-darkGray-800 select-none flex">
+        <div className="bg-darkGray-900 flex flex-col p-2 width-subSidebar"></div>
+        <div className="flex flex-col flex-1 width-sidebar ">
+          <div className="height-header flex w-full p-3 py-2 border-b-2 border-darkGray-900">
+            <input
+              type="text"
+              value={this.state.findText}
+              placeholder="Find or start a conversation"
+              className="bg-darkGray-900 text-darkGray-400 placeholder-darkGray-400 text-sm px-2 rounded-md outline-none height-input m-auto w-full"
+              onChange={e => this.setState({ findText: e.target.value })}
+            />
+          </div>
+          <div className="p-3 pt-1 px-2">
+            <span className="text-xs px-3 mt-2 text-darkGray-400 font-medium">
+              DIRECT MESSAGES
+            </span>
+            <UserRow />
+          </div>
+          <div className="bg-darkGray-850 p-3 m-auto mb-0 w-full">
+            <UserDock />
+          </div>
         </div>
       </div>
     );
