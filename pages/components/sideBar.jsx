40d249e138d37c6552a8e486c5da6b0223dbdb90
blackstormx <blackshtormx@gmail.com>
2020-05-22 20:29:56 +0400

    Update sideBar.jsx

--- pages/components/sideBar.jsx
+++ pages/components/sideBar.jsx
@@ -1,5 +1,18 @@
 import React, { Component } from "react";
 
+class UserDock extends Component {
+  render() {
+    return (
+      <div className="flex cursor-pointer px-1 rounded-md">
+        <div className="user-head-small"></div>
+        <div className="text-darkGray-100 font-medium flex-1 flex">
+          <span className="m-auto ml-4">White_Stardust</span>
+        </div>
+      </div>
+    );
+  }
+}
+
 class UserRow extends Component {
   render() {
     return (
@@ -29,11 +42,14 @@ class SideBar extends Component {
             onChange={e => this.setState({ findText: e.target.value })}
           />
         </div>
-        <span className="text-xs px-4 mt-2 text-darkGray-400 font-medium">
-          DIRECT MESSAGES
-        </span>
-        <div className="p-3 pt-1 px-1">
+        <div className="p-3 pt-1 px-2">
+          <span className="text-xs px-3 mt-2 text-darkGray-400 font-medium">
+            DIRECT MESSAGES
+          </span>
           <UserRow />
+        </div>
+        <div className="bg-darkGray-900 p-3 m-auto mb-0 w-full">
+          <UserDock />
         </div>
       </div>
     );
