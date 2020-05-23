ce1b45360860a3fd3ce4d3c8cd36b31719b1023f
blackstormx <blackshtormx@gmail.com>
2020-05-23 20:25:59 +0400

    refractoring

---
+++ pages/components/settings/settings.jsx
@@ -0,0 +1,47 @@
+import React, { Component } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
+
+class Settings extends Component {
+  render() {
+    return (
+      <div
+        className={
+          "absolute top-0 left-0 w-full bg-darkGray-700 flex h-screen overflow-hidden ease duration-150 transition-all" +
+          (this.props.mainPageVisible
+            ? " opacity-0 scale-anim-115 pointer-events-none"
+            : " opacity-100 scale-anim-100")
+        }
+      >
+        <div className="flex w-full h-full">
+          <div className="flex-settings-left flex bg-darkGray-800 justify-end pt-20">
+            <div className="width-settings-sidebar">
+              <span className="text-darkGray-400 text-xs font-bold">
+                USER SETTINGS
+              </span>
+            </div>
+          </div>
+          <div className="flex-settings-right flex justify-start pt-20">
+            <div className="px-10 min-w-740 text-darkGray-100 font-bold">
+              MY ACCOUNT
+            </div>
+            <div className="px-10">
+              <button
+                className="content-center flex flex-col outline-none focus:outline-none"
+                onClick={this.props.closeSettings}
+              >
+                <span className="rounded-full text-darkGray-200 hover:bg-darkGray-650 cursor-pointer border-2 border-darkGray-500 wh-square-md flex">
+                  <FontAwesomeIcon icon={faPlus} className="m-auto" />
+                </span>
+                <span className="text-darkGray-500 text-xs font-bold mx-auto mt-1">
+                  ESC
+                </span>
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+export default Settings;
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import SideBar from "./components/sideBar";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPlus } from "@fortawesome/free-solid-svg-icons";
+import Settings from "./components/settings/settings";
 
 const cURL = "http://localhost:2999";
 
@@ -34,42 +33,10 @@ class Index extends Component {
         >
           <SideBar user={this.state.user} openSettings={this.openSettings} />
         </div>
-        <div
-          className={
-            "absolute top-0 left-0 w-full bg-darkGray-700 flex h-screen overflow-hidden ease duration-150 transition-all" +
-            (this.state.mainPageVisible
-              ? " opacity-0 scale-anim-115 pointer-events-none"
-              : " opacity-100 scale-anim-100")
-          }
-        >
-          <div className="flex w-full h-full">
-            <div className="flex-settings-left flex bg-darkGray-800 justify-end pt-20">
-              <div className="width-settings-sidebar">
-                <span className="text-darkGray-400 text-xs font-bold">
-                  USER SETTINGS
-                </span>
-              </div>
-            </div>
-            <div className="flex-settings-right flex justify-start pt-20">
-              <div className="px-10 min-w-740 text-darkGray-100 font-bold">
-                MY ACCOUNT
-              </div>
-              <div className="px-10">
-                <button
-                  className="content-center flex flex-col outline-none focus:outline-none"
-                  onClick={this.closeSettings}
-                >
-                  <span className="rounded-full text-darkGray-200 hover:bg-darkGray-650 cursor-pointer border-2 border-darkGray-500 wh-square-md flex">
-                    <FontAwesomeIcon icon={faPlus} className="m-auto" />
-                  </span>
-                  <span className="text-darkGray-500 text-xs font-bold mx-auto mt-1">
-                    ESC
-                  </span>
-                </button>
-              </div>
-            </div>
-          </div>
-        </div>
+        <Settings
+          mainPageVisible={this.state.mainPageVisible}
+          closeSettings={this.closeSettings}
+        />
       </div>
     );
   }
