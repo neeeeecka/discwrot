9a573cdb862d4d928255480cb4c57893343a8f44
blackstormx <blackshtormx@gmail.com>
2020-05-23 23:12:10 +0400

    page select

---
+++ pages/components/settings/pages/myAccount.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+class MyAccount extends Component {
+  render() {
+    return (
+      <div>
+        <span>First page</span>
+      </div>
+    );
+  }
+}
+export default MyAccount;
--- pages/components/settings/settings.jsx
+++ pages/components/settings/settings.jsx
@@ -1,8 +1,47 @@
 import React, { Component } from "react";
+import loadable from "@loadable/component";
+
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPlus } from "@fortawesome/free-solid-svg-icons";
 
 class Settings extends Component {
+  state = {
+    selectedPage: "myAccount",
+    settings: { myAccount: "My Account", privacy: "Privacy & Safety" }
+  };
+
+  selectPage = page => {
+    this.setState({ selectedPage: page });
+  };
+  getLoadedPage = () => {
+    const LoadedPage = loadable(() =>
+      import("./pages/" + this.state.selectedPage)
+    );
+    return <LoadedPage />;
+  };
+  getPageButtons = () => {
+    const keys = Object.keys(this.state.settings);
+    const dom = [];
+
+    keys.forEach(page => {
+      const title = this.state.settings[page];
+      dom.push(
+        <button
+          className={
+            "w-full text-left mb-1 px-2 py-1 rounded-sm outline-none focus:outline-none" +
+            (page === this.state.selectedPage
+              ? " bg-darkGray-600 text-darkGray-100"
+              : " text-darkGray-200 hover:bg-darkGray-700")
+          }
+          onClick={() => this.selectPage(page)}
+        >
+          <span>{title}</span>
+        </button>
+      );
+    });
+    return dom;
+  };
+
   render() {
     return (
       <div
@@ -15,15 +54,17 @@ class Settings extends Component {
       >
         <div className="flex w-full h-full">
           <div className="flex-settings-left flex bg-darkGray-800 justify-end pt-16 pl-6">
-            <div className="width-settings-sidebar">
-              <span className="text-darkGray-400 text-xs font-bold">
+            <div className="width-settings-sidebar pr-3">
+              <span className="flex flex-col text-darkGray-400 text-xs font-bold mb-1 ml-2">
                 USER SETTINGS
               </span>
+              {this.getPageButtons()}
             </div>
           </div>
           <div className="flex-settings-right flex justify-start pt-16">
-            <div className="px-10 min-w-acc text-darkGray-100 font-bold">
-              MY ACCOUNT
+            <div className="px-10 min-w-acc">
+              <span className="text-darkGray-100 font-bold">MY ACCOUNT</span>
+              <div>{this.getLoadedPage()}</div>
             </div>
             <div className="px-10 pl-0">
               <button
