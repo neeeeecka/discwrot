d7f2e7d9a316fc9873c0fceb4c76fd518d41ded4
blackstormx <blackshtormx@gmail.com>
2020-05-23 20:16:57 +0400

    des

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import SideBar from "./components/sideBar";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
 
 const cURL = "http://localhost:2999";
 
@@ -38,10 +40,28 @@ class Index extends Component {
           }
         >
           <div className="flex w-full h-full">
-            <div className="flex-settings-left flex bg-darkGray-800 justify-end">
-              <div className="width-settings-sidebar">tst</div>
+            <div className="flex-settings-left flex bg-darkGray-800 justify-end pt-20">
+              <div className="width-settings-sidebar">
+                <span className="text-darkGray-400 text-xs font-bold">
+                  USER SETTINGS
+                </span>
+              </div>
             </div>
-            <div className="flex-settings-right flex justify-start">test</div>
+            <div className="flex-settings-right flex justify-start pt-20">
+              <div className="px-10 min-w-740 text-darkGray-100 font-bold">
+                MY ACCOUNT
+              </div>
+              <div className="px-10">
+                <div className="content-center flex flex-col">
+                  <span className="rounded-full text-darkGray-200 hover:bg-darkGray-650 cursor-pointer border-2 border-darkGray-500 wh-square-md flex">
+                    <FontAwesomeIcon icon={faPlus} className="m-auto" />
+                  </span>
+                  <span className="text-darkGray-500 text-xs font-bold mx-auto mt-1">
+                    ESC
+                  </span>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
