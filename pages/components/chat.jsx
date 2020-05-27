7df06f16224a43db17e9fb595b86aeddfc2e656c
blackstormx <blackshtormx@gmail.com>
2020-05-27 18:32:41 +0400

    Update chat.jsx

--- pages/components/chat.jsx
+++ pages/components/chat.jsx
@@ -7,6 +7,18 @@ import {
 } from "@fortawesome/free-solid-svg-icons";
 import loadable from "@loadable/component";
 
+const FlatNonContent = props => (
+  <div className="py-0.5 my-3 pl-4 text-base text-darkGray-100 hover:bg-darkGray-750 flex">
+    <div className="w-40px m-auto ml-0 mr-4 flex">{props.icon}</div>
+    <div>
+      <span className="hover:underline cursor-pointer">
+        {props.message.author.username}
+      </span>
+      <span className="text-darkGray-400"> {props.children}</span>
+    </div>
+  </div>
+);
+
 class Chat extends Component {
   state = {
     message: "",
@@ -68,40 +80,34 @@ class Chat extends Component {
       } else {
         if (message.call) {
           dom.unshift(
-            <div className="py-0.5 my-3 pl-4 text-base text-darkGray-100 hover:bg-darkGray-750 flex">
-              <div className="w-40px m-auto ml-0 mr-4 flex">
+            <FlatNonContent
+              message={message}
+              icon={
                 <FontAwesomeIcon
                   icon={faPhone}
                   className="text-green-500 m-auto sml-4"
                   style={{ transform: "rotate(90deg)" }}
                 />
-              </div>
-              <div>
-                <span className="hover:underline cursor-pointer">
-                  {message.author.username}
-                </span>
-                <span className="text-darkGray-400"> started a call</span>
-              </div>
-            </div>
+              }
+            >
+              started a call
+            </FlatNonContent>
           );
         }
         if (message.attachments.length) {
           dom.unshift(
-            <div className="py-0.5 my-3 pl-4 text-base text-darkGray-100 hover:bg-darkGray-750 flex">
-              <div className="w-40px m-auto ml-0 mr-4 flex">
+            <FlatNonContent
+              message={message}
+              icon={
                 <FontAwesomeIcon
                   icon={faFileDownload}
                   className="text-accent-500 m-auto sml-4 cursor-pointer"
                   //   style={{ transform: "rotate(90deg)" }}
                 />
-              </div>
-              <div>
-                <span className="hover:underline cursor-pointer">
-                  {message.author.username}
-                </span>
-                <span className="text-darkGray-400"> sent an attachment</span>
-              </div>
-            </div>
+              }
+            >
+              sent an attachment
+            </FlatNonContent>
           );
         }
       }
