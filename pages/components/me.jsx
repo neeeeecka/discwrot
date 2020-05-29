f942f3a5788f6073811888cae84a409dc8614e5a
blackstormx <blackshtormx@gmail.com>
2020-05-29 10:43:43 +0400

    add friend

--- pages/components/me.jsx
+++ pages/components/me.jsx
@@ -6,13 +6,16 @@ class Me extends Component {
     return (
       <React.Fragment>
         <div className="height-header flex w-full px-5 py-2 border-b shadow-bottom border-darkGray-900">
-          <span>
+          <span className="pt-0.5">
             <FontAwesomeIcon
               icon={faUserFriends}
               className="my-auto text-darkGray-300 mr-2"
             />
             <span className="font-bold text-darkGray-100">Friends</span>
           </span>
+          <button className="focus:outline-none text-darkGray-100 text-sm font-bold leading-none rounded-md px-2 ml-6 bg-accent-600">
+            Add Friend
+          </button>
         </div>
       </React.Fragment>
     );
